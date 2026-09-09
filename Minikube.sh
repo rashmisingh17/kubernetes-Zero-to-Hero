d5e0d9b96468888d6ec916cf012daaa6ec320b82
rashmisingh17 <rashmi.singh17k@gmail.com>
2026-09-10 00:24:39 +0530

    Create Minikube.sh

---
+++ Minikube.sh
@@ -0,0 +1,9 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+chmod +x minikube-linux-amd64
+
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
+
+minikube version   
+# start cluster
+minikube start --driver=docker
