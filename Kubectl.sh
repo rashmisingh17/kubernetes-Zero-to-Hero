521555c6485d32a7d487fe47cbfb10decddb3285
rashmisingh17 <rashmi.singh17k@gmail.com>
2026-09-10 21:45:47 +0530

    Update Kubectl.sh

--- Kubectl.sh
+++ Kubectl.sh
@@ -1,11 +1,9 @@
 ARCH=$(uname -m)
+VERSION=$(curl -L -s https://k8s.io)
 
-VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
-
-curl -LO "https://dl.k8s.io/release/${VERSION}/bin/linux/amd64/kubectl"
+curl -LO "https://k8s.io{VERSION}/bin/linux/amd64/kubectl"
 
 chmod +x kubectl
-
 sudo mv kubectl /usr/local/bin/kubectl
 
 kubectl version --client
